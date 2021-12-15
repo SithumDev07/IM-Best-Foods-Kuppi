af62bb9f695e546641134a2c6a17f0979dc83288
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-12-16 01:42:02 +0530

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,46 @@
+//* Var Example
+/*var greeting = "Say Hello!"
+
+var times = 4
+
+if (times > 3){
+    var greeting  = "Say Chao!"
+}
+
+console.log(greeting); */
+
+
+//* Let Example
+
+let greeting = "Say Hello!"
+
+//let greeting = 456
+
+var times = 4
+
+if (times > 3) { let greeting = "Say Chao!"; }
+
+console.log(greeting)
+
+//* Const Example
+
+const greeting = "Say Hello"
+
+//* greeting = "Say Chao!"
+
+//* const greeting = "Say Ayubowan!"
+
+const greeting = {
+    name: "IM Best Foods",
+    tagline: "Experience the freshness"
+}
+
+/*
+greeting = {
+    name: "IM Best",
+    tagline: "Try before die."
+}
+*/
+
+greeting.name = "IM Best"
+greeting.tagline = "Try before die"
