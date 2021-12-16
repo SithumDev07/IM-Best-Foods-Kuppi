47da4429909ae138eb2292b500599af09fe15fe4
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-12-16 10:09:29 +0530

    Added Js files

---
+++ data/data.js
@@ -0,0 +1,51 @@
+const foods = [
+    {
+        name: "Argular Salad",
+        image: "./images/food1.png",
+        ingredients: "Green Apple, Goat Cheese",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.",
+        nutritionals: {
+            kalories: "103Kal",
+            fat: "10g",
+            carbs: "40g"
+        },
+        price: 350.00
+    },
+    {
+        name: "Noodle Egg Vegie",
+        image: "./images/food2.png",
+        ingredients: "Salmon, Eggs, Chinese Noodles",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.",
+        nutritionals: {
+            kalories: "320Kal",
+            fat: "45g",
+            carbs: "80g"
+        },
+        price: 485.00
+    },
+    {
+        name: "Potato Quinoa Patties",
+        image: "./images/food3.png",
+        ingredients: "Potatoes, Salmon",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.",
+        nutritionals: {
+            kalories: "220Kal",
+            fat: "45g",
+            carbs: "95g"
+        },
+        price: 750.00
+    },
+    {
+        name: "Hot Shakshuka",
+        image: "./images/food4.png",
+        ingredients: "Eggs, Tomatoes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.",
+        nutritionals: {
+            kalories: "86Kal",
+            fat: "12g",
+            carbs: "50g"
+        },
+        price: 445.00
+    },
+
+]
---
+++ scripts/product-opener.js
@@ -0,0 +1,59 @@
+const foodNames = ["Argular Salad", "Noodle Egg Vegie", "Potato Quinoa Patties", "Hot Shakshuka"]
+
+const ingredients = ["Green Apple, Goat Cheese", "Salmon, Eggs, Chinese Noodles", "Potatoes, Salmon", "Eggs, Tomatoes"]
+
+const descriptions = [
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.",
+
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.", 
+
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima.", 
+
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima recusandae quaerat labore consequuntur officiis, harum ipsam necessitatibus sequi optio omnis consequatur voluptatibus quas ratione perspiciatis eos unde quisquam voluptatum placeat? Deleniti, eaque consectetur? Corrupti, pariatur quae voluptatibus nisi debitis nam voluptatem consectetur atque quas a iste officiis ducimus quam laborum, eum ipsa soluta laudantium, aliquam doloribus odio? Magnam eveniet, at architecto doloribus facilis minima."
+]
+
+const prices = [350.00, 485.00, 750.00, 445.00]
+
+const images = ["./images/food1.png", "./images/food2.png", "./images/food3.png", "./images/food4.png"]
+
+const foodsContainer = document.querySelector('.foods')
+
+
+// for(let i = 0; i < 4; i++){
+
+//     const foodCard = document.createElement('div');
+//     foodCard.classList.add('food-card')
+//     foodCard.innerHTML = `
+//         <img src="${images[i]}" alt="Food Items" />
+//         <h3>${foodNames[i]}</h3>
+//         <p>With ${ingredients[i]}</p>
+//         <p class="price">Rs.${prices[i]}</p>
+//     `;
+//     foodsContainer.appendChild(foodCard)
+
+// }
+
+
+//* Animations
+const signup = document.querySelector('#signup')
+
+const main = document.querySelector('main')
+const preloader = document.querySelector('.product-preload')
+const productPage = document.querySelector('.product-page')
+const preloaderYellow = document.querySelector('.product-preload-yellow')
+
+signup.addEventListener('click', () => {
+
+    main.classList.toggle('beforeLoadLeft')
+    main.classList.toggle('afterLoad')
+    
+    preloader.classList.toggle('beforeLoad');
+    preloader.classList.toggle('afterLoad')
+
+    productPage.classList.toggle('beforeLoad')
+    productPage.classList.toggle('afterLoad')
+
+    preloaderYellow.classList.toggle('beforeLoad')
+    preloaderYellow.classList.toggle('afterLoad')
+
+})
